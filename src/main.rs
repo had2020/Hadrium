4f6a582e403f723acfe72bdc7ee546925ecea4fd
Hadrian Lazic <59424667+had2020@users.noreply.github.com>
2025-06-28 14:40:57 -0700

    update

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
-use ::TerimalRtdm::*;
+use TerimalRtdm::*;
+
+use std::env;
 
 use std::io::{stdout, Write}; //TODO Usage
 
@@ -21,144 +23,151 @@ enum PrimaryMode {
 }
 
 fn main() {
-    let mut primary_mode: PrimaryMode = PrimaryMode::NormalMode;
+    let args: Vec<String> = env::args().collect();
 
-    let mut app = App::new();
-    clear(&mut app);
+    if args.len() > 1 {
+        let mut primary_mode: PrimaryMode = PrimaryMode::NormalMode;
+        let mut app = App::new();
 
-    raw_line("Welcome to Hadrium");
+        raw_mode(true);
+        clear(&mut app);
 
-    raw_mode(true);
-    clear(&mut app);
+        app.enable_f_row_and_arrow = true;
 
-    app.enable_f_row_and_arrow = true;
+        // startup
+        app.enable_f_row_and_arrow = true;
+        Text::new()
+            .foreground(Color::Black)
+            .background(Color::Green)
+            .style(Style::Bold)
+            .show(&mut app, "Normal", pos!(0, 0));
 
-    loop {
-        collect_presses(&mut app);
+        loop {
+            // Normal mode
+            if Key::o().pressed(&mut app, KeyType::Esc) {
+                app.enable_f_row_and_arrow = true;
+                primary_mode = PrimaryMode::NormalMode;
+                Text::new()
+                    .foreground(Color::Black)
+                    .background(Color::Green)
+                    .style(Style::Bold)
+                    .show(&mut app, "Normal", pos!(0, 0));
+            }
 
-        //key_pressed(&app);
+            // Insert mode
+            if Key::o().pressed(&mut app, KeyType::i) {
+                app.enable_f_row_and_arrow = true;
+                primary_mode = PrimaryMode::InsertMode;
+                Text::new()
+                    .foreground(Color::Black)
+                    .background(Color::Red)
+                    .style(Style::Bold)
+                    .show(&mut app, "Insert", pos!(0, 0));
 
-        // Normal mode
-        if Key::o().pressed(&mut app, KeyType::Esc) {
-            app.enable_f_row_and_arrow = true;
-            primary_mode = PrimaryMode::NormalMode;
-            Text::new()
-                .foreground(Color::Black)
-                .background(Color::Green)
-                .style(Style::Bold)
-                .show(&mut app, "Normal", pos!(0, 0));
+                // written text TODO
+                Text::new()
+                    .vanish(false)
+                    .show(&mut app, "Test1", pos!(0, 1));
+                Text::new()
+                    .vanish(false)
+                    .show(&mut app, "Test2", pos!(0, 2));
+            }
+
+            // Visual mode
+            if Key::o().case_sen(true).pressed(&mut app, KeyType::v) {
+                app.enable_f_row_and_arrow = false;
+                primary_mode = PrimaryMode::VisualMode;
+                Text::new()
+                    .foreground(Color::Black)
+                    .background(Color::Blue)
+                    .style(Style::Bold)
+                    .show(&mut app, "Visual", pos!(0, 0));
+            }
+
+            // Command mode
+            if Key::o().pressed(&mut app, KeyType::Colon) {
+                app.enable_f_row_and_arrow = false;
+                primary_mode = PrimaryMode::CommandLineMode;
+                Text::new()
+                    .foreground(Color::Black)
+                    .background(Color::Yellow)
+                    .style(Style::Bold)
+                    .show(&mut app, "Command", pos!(0, 0));
+            }
+
+            if primary_mode == PrimaryMode::CommandLineMode {
+                if Key::o().pressed(&mut app, KeyType::q) {
+                    clear(&mut app);
+                    break;
+                }
+            }
+
+            // Replace mode
+            if Key::o().pressed(&mut app, KeyType::R) {
+                app.enable_f_row_and_arrow = false;
+                primary_mode = PrimaryMode::ReplaceMode;
+                Text::new()
+                    .foreground(Color::Black)
+                    .background(Color::Magenta)
+                    .style(Style::Bold)
+                    .show(&mut app, "Replace", pos!(0, 0));
+            }
+
+            if primary_mode == PrimaryMode::NormalMode {
+                // Vim directional
+                if Key::o().no_clear().pressed(&mut app, KeyType::k) {
+                    Mov::cur().dir(&mut app, Dir::Up, 1); //Todo block impl of wrap
+                }
+                if Key::o().no_clear().pressed(&mut app, KeyType::j) {
+                    Mov::cur().dir(&mut app, Dir::Down, 1);
+                }
+                if Key::o().no_clear().pressed(&mut app, KeyType::h) {
+                    Mov::cur().dir(&mut app, Dir::Left, 1);
+                }
+                if Key::o().no_clear().pressed(&mut app, KeyType::l) {
+                    Mov::cur().dir(&mut app, Dir::Right, 1);
+                }
+
+                // Arrow directional
+                if Key::o().no_clear().pressed(&mut app, KeyType::UpArrow) {
+                    Mov::cur().block().dir(&mut app, Dir::Up, 1);
+                }
+                if Key::o().no_clear().pressed(&mut app, KeyType::DownArrow) {
+                    Mov::cur().block().dir(&mut app, Dir::Down, 1);
+                }
+                if Key::o().no_clear().pressed(&mut app, KeyType::LeftArrow) {
+                    Mov::cur().block().dir(&mut app, Dir::Left, 1);
+                }
+                if Key::o().no_clear().pressed(&mut app, KeyType::RightArrow) {
+                    Mov::cur().block().dir(&mut app, Dir::Right, 1);
+                }
+            }
+
+            if primary_mode == PrimaryMode::InsertMode {
+                //println!("{:?}", key_pressed(&app));
+                //println!("{:?}", debug_code_pressed(&mut app));
+
+                // Arrow directional with wrap
+                if Key::o().no_clear().pressed(&mut app, KeyType::UpArrow) {
+                    Mov::cur().wrap().dir(&mut app, Dir::Up, 1);
+                }
+                if Key::o().no_clear().pressed(&mut app, KeyType::DownArrow) {
+                    Mov::cur().wrap().dir(&mut app, Dir::Down, 1);
+                }
+                if Key::o().no_clear().pressed(&mut app, KeyType::LeftArrow) {
+                    Mov::cur().wrap().dir(&mut app, Dir::Left, 1);
+                }
+                if Key::o().no_clear().pressed(&mut app, KeyType::RightArrow) {
+                    Mov::cur().wrap().dir(&mut app, Dir::Right, 1);
+                }
+            }
+
+            render(&app);
+            collect_presses(&mut app);
         }
 
-        // Insert mode
-        if Key::o().pressed(&mut app, KeyType::i) {
-            app.enable_f_row_and_arrow = true;
-            primary_mode = PrimaryMode::InsertMode;
-            Text::new()
-                .foreground(Color::Black)
-                .background(Color::Red)
-                .style(Style::Bold)
-                .show(&mut app, "Insert", pos!(0, 0));
-
-            // written text TODO
-            Text::new()
-                .vanish(false)
-                .show(&mut app, "Test1", pos!(0, 1));
-            Text::new()
-                .vanish(false)
-                .show(&mut app, "Test2", pos!(0, 2));
-        }
-
-        // Visual mode
-        if Key::o().case_sen(true).pressed(&mut app, KeyType::v) {
-            app.enable_f_row_and_arrow = false;
-            primary_mode = PrimaryMode::VisualMode;
-            Text::new()
-                .foreground(Color::Black)
-                .background(Color::Blue)
-                .style(Style::Bold)
-                .show(&mut app, "Visual", pos!(0, 0));
-        }
-
-        // Command mode
-        if Key::o().pressed(&mut app, KeyType::Colon) {
-            app.enable_f_row_and_arrow = false;
-            primary_mode = PrimaryMode::CommandLineMode;
-            Text::new()
-                .foreground(Color::Black)
-                .background(Color::Yellow)
-                .style(Style::Bold)
-                .show(&mut app, "Command", pos!(0, 0));
-        }
-
-        if primary_mode == PrimaryMode::CommandLineMode {
-            if Key::o().pressed(&mut app, KeyType::q) {
-                clear(&mut app);
-                break;
-            }
-        }
-
-        // Replace mode
-        if Key::o().pressed(&mut app, KeyType::R) {
-            app.enable_f_row_and_arrow = false;
-            primary_mode = PrimaryMode::ReplaceMode;
-            Text::new()
-                .foreground(Color::Black)
-                .background(Color::Magenta)
-                .style(Style::Bold)
-                .show(&mut app, "Replace", pos!(0, 0));
-        }
-
-        if primary_mode == PrimaryMode::NormalMode {
-            // Vim directional
-            if Key::o().no_clear().pressed(&mut app, KeyType::k) {
-                Mov::cur().dir(&mut app, Dir::Up, 1); //Todo block impl of wrap
-            }
-            if Key::o().no_clear().pressed(&mut app, KeyType::j) {
-                Mov::cur().dir(&mut app, Dir::Down, 1);
-            }
-            if Key::o().no_clear().pressed(&mut app, KeyType::h) {
-                Mov::cur().dir(&mut app, Dir::Left, 1);
-            }
-            if Key::o().no_clear().pressed(&mut app, KeyType::l) {
-                Mov::cur().dir(&mut app, Dir::Right, 1);
-            }
-
-            // Arrow directional
-            if Key::o().no_clear().pressed(&mut app, KeyType::UpArrow) {
-                Mov::cur().block().dir(&mut app, Dir::Up, 1);
-            }
-            if Key::o().no_clear().pressed(&mut app, KeyType::DownArrow) {
-                Mov::cur().block().dir(&mut app, Dir::Down, 1);
-            }
-            if Key::o().no_clear().pressed(&mut app, KeyType::LeftArrow) {
-                Mov::cur().block().dir(&mut app, Dir::Left, 1);
-            }
-            if Key::o().no_clear().pressed(&mut app, KeyType::RightArrow) {
-                Mov::cur().block().dir(&mut app, Dir::Right, 1);
-            }
-        }
-
-        if primary_mode == PrimaryMode::InsertMode {
-            //println!("{:?}", key_pressed(&app));
-            //println!("{:?}", debug_code_pressed(&mut app));
-
-            // Arrow directional with wrap
-            if Key::o().no_clear().pressed(&mut app, KeyType::UpArrow) {
-                Mov::cur().wrap().dir(&mut app, Dir::Up, 1);
-            }
-            if Key::o().no_clear().pressed(&mut app, KeyType::DownArrow) {
-                Mov::cur().wrap().dir(&mut app, Dir::Down, 1);
-            }
-            if Key::o().no_clear().pressed(&mut app, KeyType::LeftArrow) {
-                Mov::cur().wrap().dir(&mut app, Dir::Left, 1);
-            }
-            if Key::o().no_clear().pressed(&mut app, KeyType::RightArrow) {
-                Mov::cur().wrap().dir(&mut app, Dir::Right, 1);
-            }
-        }
-
-        render(&app);
+        raw_mode(false);
+    } else {
+        println!("Please provide a filename argument!");
     }
-
-    raw_mode(false);
 }
